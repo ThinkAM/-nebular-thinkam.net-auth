1163826e1ff42743dc278595a2f07b3803739b5f
Felipe A. de Almeida <felipe.almeida@thinkam.net>
2017-12-29 21:57:09 -0200

    Passado por parametro a URL de redirecionamento após a inserção

--- providers/email-pass-auth.provider.ts
+++ providers/email-pass-auth.provider.ts
@@ -241,7 +241,7 @@ export class NbEmailPassAuthProvider extends NbAbstractAuthProvider {
         return res;
       })
       .map((res: HttpResponse<NbAuthResult>) => {
-        return new NbAuthResult(res.body["success"], res, null, res.body["errors"], res.body["messages"], null);
+        return new NbAuthResult(res.body["success"], res, "http://thinkam.net/beta/", res.body["errors"], res.body["messages"], null);
       })
       .catch((res) => {
         let errors = [];
